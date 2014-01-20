24462349fd4848544e32a5b1ea69c7f48c698bc5
manish kangia <manishkangia@manishs-MacBook-Pro.local>
2014-01-20 17:24:09 +0530

    adding generator file and renaming payment method partial correctly

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -12,7 +12,7 @@ Spree::CheckoutController.class_eval do
   def redirect_for_card_payment
     payment_method_id = params[:order][:payments_attributes][0][:payment_method_id] if params[:order] && params[:order][:payments_attributes]
     payment_method = Spree::PaymentMethod.where(:id => payment_method_id).first
-    if payment_method.try(:type) == 'Spree::PaymentMethod::UnifiedPayment'
+    if payment_method.try(:type) == 'Spree::PaymentMethod::UnifiedPaymentMethod'
       session[:transaction_id] = generate_transaction_id
       @order.update_attributes(object_params)
       redirect_to new_unified_transaction_path
