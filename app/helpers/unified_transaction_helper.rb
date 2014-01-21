59fd0cc00494f87fc55937dedc900f0df64eee7f
manish kangia <manishkangia@manishs-MacBook-Pro.local>
2014-01-21 12:10:37 +0530

    not set session in checkout controller for card payment as it will be set in new method

--- app/helpers/unified_transaction_helper.rb
+++ app/helpers/unified_transaction_helper.rb
@@ -3,13 +3,6 @@ module UnifiedTransactionHelper
     (amount.to_f)*100
   end
 
-  # def create_card_transaction(order, response, transaction_id)
-  #   response_order = response['Order']
-  #   gateway_transaction = UnifiedPayment::Transaction.where(:gateway_session_id => response_order['SessionID'], :gateway_order_id => response_order['OrderID'], :url => response_order['URL']).first
-  #   gateway_transaction.build_card_transaction(:user_id => order.user.try(:id), :payment_transaction_id => transaction_id, :order_id => order.id, :gateway_order_status => 'CREATED', :amount => order.total, :currency => Spree::Config[:currency], :xml_response => response.inspect, :response_status => response["Status"], :status => 'pending')
-  #   gateway_transaction.save!
-  # end
-
   def generate_transaction_id
     (1..3).each do |attempt|
       @transaction_id = generate_id_using_timestamp + attempt.to_s
