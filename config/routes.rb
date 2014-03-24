0579d677b96ad4c4f8258b4fa3e49ab2885b55ea
manish kangia <manishkangia@manishs-MacBook-Pro.local>
2014-03-24 12:36:00 +0530

    update routes.rb for routes updated format to use draw instead of prepend

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
-Spree::Core::Engine.routes.prepend do
-  get '/unified_payments', :to => "unified_payments#index", :as => :unified_payments
-  get '/unified_payments/new', :to => "unified_payments#new", :as => :new_unified_transaction
-  post '/unified_payments/create', :to => "unified_payments#create", :as => :create_unified_transaction
-  post '/unified_payments/canceled', :to => "unified_payments#canceled", :as => :canceled_unified_payments
-  post '/unified_payments/declined', :to => "unified_payments#declined", :as => :declined_unified_payments
-  post '/unified_payments/approved', :to => "unified_payments#approved", :as => :approved_unified_payments
-  get '/unified_payments/declined', :to => redirect{ |p, request| '/' }
-
-  namespace :admin do
-    get '/unified_payments', :to => "unified_payments#index"
-    get '/unified_payments/receipt/:transaction_id', :to => "unified_payments#receipt", :as => :unified_payments_receipt
-    post '/unified_payments/query_gateway', :to => "unified_payments#query_gateway", :as => :unified_payments_query_gateway
-  end
+Spree::Core::Engine.routes.draw do
+  get '/unified_payments' => "unified_payments#index", as: :unified_payments
+  get '/unified_payments/new' => "unified_payments#new", as: :new_unified_transaction
+  post '/unified_payments/create' => "unified_payments#create", as: :create_unified_transaction
+  post '/unified_payments/canceled' => "unified_payments#canceled", as: :canceled_unified_payments
+  get '/unified_payments/declined' => "unified_payments#declined", as: :declined_unified_payments
+  post '/unified_payments/approved' => "unified_payments#approved", as: :approved_unified_payments
+  
+  get 'admin/unified_payments' => "admin/unified_payments#index"
+  get 'admin/unified_payments/receipt/:transaction_id' => "admin/unified_payments#receipt", as: :unified_payments_receipt
+  post 'admin/unified_payments/query_gateway' => "admin/unified_payments#query_gateway", as: :unified_payments_query_gateway
+  
+  #for test
+  #[TODO]:uncomment this later 
+  # get '/unified_payments/declined', :to => redirect{ |p, request| '/' }
+  
+  #[TODO]:remove this later
+  # get '/unified_payments/approved', :to => "unified_payments#approved"
+  # get '/unified_payments/canceled', :to => "unified_payments#canceled"
 end
