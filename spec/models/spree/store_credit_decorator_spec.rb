a119d860213cf353a9ccc9ca6e8b6ecbac1f9d26
manish kangia <manishkangia@manishs-MacBook-Pro.local>
2014-03-26 20:23:25 +0530

    remove specs for mass assignment for store credit class

--- spec/models/spree/store_credit_decorator_spec.rb
+++ spec/models/spree/store_credit_decorator_spec.rb
@@ -1,11 +1,5 @@
 require 'spec_helper'
 
 describe Spree::StoreCredit do
-  describe 'attr-accessible' do
-    [:balance, :user, :transactioner, :type].each do |attribute|
-      it { should allow_mass_assignment_of attribute }
-    end
-  end
-
   it { should belong_to(:unified_transaction).class_name('UnifiedPayment::Transaction').with_foreign_key('unified_transaction_id') }
 end
