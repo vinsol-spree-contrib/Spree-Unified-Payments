0f14bc98226142b5094d3a0783564d579b30f675
manish kangia <manishkangia@manishs-MacBook-Pro.local>
2014-03-28 14:58:10 +0530

    remove unwanted routes and add the one needed to declined for get request

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,6 @@ Spree::Core::Engine.routes.draw do
   get 'admin/unified_payments' => "admin/unified_payments#index"
   get 'admin/unified_payments/receipt/:transaction_id' => "admin/unified_payments#receipt", as: :admin_unified_payments_receipt
   post 'admin/unified_payments/query_gateway' => "admin/unified_payments#query_gateway", as: :admin_unified_payments_query_gateway
-  
-  #for test
-  #[TODO]:uncomment this later 
-  # get '/unified_payments/declined', :to => redirect{ |p, request| '/' }
-  
-  #[TODO]:remove this later
-  # get '/unified_payments/approved', :to => "unified_payments#approved"
-  # get '/unified_payments/canceled', :to => "unified_payments#canceled"
+   
+  get '/unified_payments/declined', :to => redirect{ |p, request| '/' }  
 end
